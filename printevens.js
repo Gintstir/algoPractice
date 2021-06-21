fc3f787b975fc368f3ced565e65b94b787d63317
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-06-21 16:42:33 -0400

    added new file printevens.js

---
+++ printevens.js
@@ -0,0 +1,14 @@
+console.log("hello");
+
+//this function prints all even numbers between two values including the values themselves
+function evenNums(a, b) {
+    for(i = a; i <= b; i++) { //classic for loop a is starting value, loop  should continue b times. incrementing once
+        if ( i % 2 === 0) { //using the modulus operator, if the value divided by two HAS a value of 0, print value)
+            console.log(i);
+        }
+        
+    }
+    //console.log(a, b)
+}
+
+evenNums(1, 15);
